2392827d58e6145b13c29498a11e7a7d72ee7c27
BadrGello <138599821+BadrGello@users.noreply.github.com>
2024-11-06 15:20:29 +0200

    Initialize

---
+++ Paint_Backend/src/main/java/com/team/Paint_Backend/PaintBackendApplication.java
@@ -0,0 +1,13 @@
+package com.team.Paint_Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaintBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaintBackendApplication.class, args);
+	}
+
+}
